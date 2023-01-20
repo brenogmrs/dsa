8b656a9a591c1e9b03e2b63b80f8f9c4e41ea9ed
brenogmrs <brenoaraujo@outlook.com.br>
2023-01-20 01:14:36 -0300

    some refactor

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -2,24 +2,20 @@ function binary_search(list, item) {
   let start = 0;
   let end = list.length - 1;
 
-  while (start <= end) {
-    let middle = Math.floor((start + end) / 2);
-
-    let guess = list[middle];
-
-    if (guess == item) {
-      return middle;
-    } else if (guess > item) {
-      end--;
+  let middle = Math.floor((start + end) / 2);
+  while (list[middle] !== item && start <= end) {
+    if (item < list[middle]) {
+      end = middle - 1;
     } else {
-      start++;
+      start = middle + 1;
     }
+
+    middle = Math.floor((start + end) / 2);
   }
 
-  return null;
+  return list[middle] === item ? middle : -1;
 }
 
 const list = [1, 3, 5, 7, 9];
 console.log(binary_search(list, 3));
 console.log(binary_search(list, -1));
-console.log(2 ** 8);
